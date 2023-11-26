484cdd8c154f51251ddd525bc98cd0b3a85c9c85
ANA-GEORGIANA ADAMESCU <ana.adamescu@stud.ubbcluj.ro>
2023-11-26 18:39:23 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Adamescu_Georgiana_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Adamescu_Georgiana_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
